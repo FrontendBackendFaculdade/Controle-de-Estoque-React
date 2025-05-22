e90036a5180be83b00a9b427e84fd2fb88c743d1
Eivold <160683610+EivoldSantos@users.noreply.github.com>
2025-05-22 11:19:43 -0300

    Adicionando Frontend

---
+++ firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+        apiKey: "AIzaSyBIOsSJmGnXlgiXIQy7g0AdUijIXgEttsQ",
+        authDomain: "dashboardiot-e21e0.firebaseapp.com",
+        projectId: "dashboardiot-e21e0",
+        storageBucket: "dashboardiot-e21e0.firebasestorage.app",
+        messagingSenderId: "752290885765",
+        appId: "1:752290885765:web:b2669e12454167b62c79ec",
+        measurementId: "G-0RQZQL96EY"
+    };
